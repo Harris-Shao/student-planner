b0e85df1ecfaba55b04dd202a0b0f6b522d62d78
hongbing.shao <hongbing@r65labs.com>
2014-11-08 11:20:41 -0500

    basic data access layout

--- studentplanner/src/edu/ecu/seng6240/team6/models/Student.java
+++ studentplanner/src/edu/ecu/seng6240/team6/models/Student.java
@@ -1,5 +1,13 @@
 package edu.ecu.seng6240.team6.models;
 
-public class Student {
+
+public class Student extends User{
+
+	public Student()
+	{
+		super();
+		super.setRole(STUDENT_ROLE);
+	}
 
 }
+
